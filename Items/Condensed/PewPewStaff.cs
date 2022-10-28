9aff2147bae083176d11ff1ec7d2071a5a4f803d
ApplesauceUwU <116694396+ApplesauceUwU@users.noreply.github.com>
2022-10-28 19:22:44 -0400

    Added new weapon

---
+++ Items/Condensed/PewPewStaff.cs
@@ -0,0 +1,59 @@
+using CrystalDreams.Items.Ammunition;
+using CrystalDreams.Projectiles.Unique;
+using Microsoft.Xna.Framework;
+using Terraria;
+using CrystalDreams.Projectiles.Unique;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System;
+using Terraria.GameContent.Creative;
+
+namespace CrystalDreams.Items.Condensed
+{
+	public class PewPewStaff : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Pew Pew Staff");
+			Tooltip.SetDefault("Basically a gun");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+		public override void SetDefaults()
+		{
+			Item.DamageType = DamageClass.Magic;
+			Item.knockBack = 0;
+			Item.damage = 12;
+			Item.rare = ItemRarityID.Blue;
+            Item.width = 48;
+            Item.height = 14;
+            Item.maxStack = 1;
+            Item.useTime = 16;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 6;
+            Item.UseSound = SoundID.Item8;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ItemType<CondensedMana>();
+            Item.useAmmo = ModContent.ItemType<CondensedMana>();
+            Item.shootSpeed = 6.6f;
+            Item.autoReuse = false;
+			
+			Item.value = Item.sellPrice(silver: 5);
+			Item.autoReuse = false;
+		}
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-1f, -2f);
+        }
+        public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Wood, 12);
+			recipe.AddIngredient<CondensedMana>(8);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
